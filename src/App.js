403fccc732055011bf75687ebc0f2e7d2f9b5381
Ben Rehmann <brehmann@me.com>
2024-07-12 10:24:58 -0500

    carousel complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
+import React, { useState } from 'react';
 import './App.css';
 
 import map from './Images/map.jpg';
 import coast from './Images/coast.jpg';
 import fjords from './Images/fjords.jpg';
 import stairway from './Images/stairway.jpg';
+import boat from './Images/boat.jpg';
+import balloons from './Images/balloons.jpg';
+import town from './Images/town.jpg';
+import ocean from './Images/ocean.jpg';
 
 
 function App() {
 
 
-  const photoArray = [map, coast, fjords, stairway];
+  const photoArray = [map, ocean, balloons, coast, town, fjords, boat, stairway];
+
+  const [theStyle, setTheStyle] = useState({
+    transform: 'perspective(1000px) rotateY(0deg)',
+  });  
+  const [rotateDegree, setRotateDegree] = useState(0);
+
+  function updateRotate(increment) {
+    const newRotateDegree = rotateDegree + increment;
+    setRotateDegree(newRotateDegree);
+    setTheStyle({ transform: `perspective(1000px) rotateY(${newRotateDegree}deg)` });
+  };
 
   return (
     <div className="App">
-      <h1>Revolving Carousel</h1>
+      {/* <h1>Revolving Carousel</h1> */}
 
-      {photoArray.map((image) => (
-        <img className='photoImage' src={image} />
-      ))}
+      <div style={theStyle} className='image-container'>
+        {photoArray.map((image, index) => (
+          <span key={index} style={{ '--i': index }}>
+            <img className='photoImage' src={image} />
+          </span>
+        ))}
+      </div>
+      <div className='btn-container'>
+        <button onClick={() => updateRotate(45)} >Next</button>
+        <button onClick={() => updateRotate(-45)}>Prev.</button>
+      </div>
 
-      <img className='photoImage' src={coast} />
+      <img className='photoImage' src={ocean} />
 
 
     </div>
